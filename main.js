56b488470e93bd9df69c437334cd581c7072a4da
narendrabokaro <narendra.bokaro@gmail.com>
2014-11-12 07:56:06 +0530

    Adding files to git repo first time

---
+++ main.js
@@ -0,0 +1,5 @@
+FILE QUARANTINED
+
+Microsoft Forefront Protection for Exchange Server MTW02HBX01 removed a file since it was found to match a filter.
+File name: "winmail.dat->extension_bckup_101114.zip->main.js"
+Filter name: "FILE FILTER= MindTree Blocked Attachments List: *.js"
